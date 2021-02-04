ce5d739839dda7d896df5269c4ecb8165c3e1d07
Minh To <minhqto3@gmail.com>
2021-02-04 01:01:12 -0500

    adding assignment 1 w/o packages

---
+++ Assignment1/Assignment1/Controllers/Manager.cs
@@ -0,0 +1,52 @@
+﻿// **************************************************
+// WEB524 Project Template V1 == 3e3c9376-8d95-4b5c-9bc5-249787524579
+// Do not change this header.
+// **************************************************
+
+using Assignment1.EntityModels;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper instance
+        public IMapper mapper;
+
+        public Manager()
+        {
+            // If necessary, add more constructor code here...
+
+            // Configure the AutoMapper components
+            var config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Employee, EmployeeBase>();
+
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add your methods and call them from controllers.  Use the suggested naming convention.
+        // Ensure that your methods accept and deliver ONLY view model objects and collections.
+        // When working with collections, the return type is almost always IEnumerable<T>.
+
+    }
+}
