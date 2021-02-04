930778983977d0c7deb28b99ae5289ae26307806
Minh To <minhqto3@gmail.com>
2021-02-04 00:29:16 -0500

    first commit

---
+++ Assignment1/Assignment1/Models/CustomerEditContactFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1.Models
+{
+    public class CustomerEditContactFormViewModel : CustomerEditContactViewModel
+    {
+
+        [Required]
+        [StringLength(40)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [StringLength(80)]
+        public string Company { get; set; }
+
+    }
+}
---
+++ Assignment1/Assignment1/Models/CustomerEditContactViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1.Models
+{
+    public class CustomerEditContactViewModel
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [StringLength(24)]
+        public string Phone { get; set; }
+        [StringLength(24)]
+        public string Fax { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string Email { get; set; }
+    }
+}
